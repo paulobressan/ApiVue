7199b3d72024a153520909b72ea6fe329289790d
Paulo bressan <paulo.bressan@outlook.com>
2018-06-10 17:59:39 -0300

    Inicio

---
+++ ApiVue/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiVue.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVue.Controllers
+{
+    [Route("api/product")]
+    [EnableCors("Politica")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var product = new List<Product>()
+            {
+                new Product("Bola de Basquete","http://avodistribuidora.com.br/wp-content/uploads/2017/10/penalty-oficialfeminino-2.jpg"),
+                new Product("Galinha Pintadinha", "http://2.bp.blogspot.com/-vq_eK8-1ops/T9eBBMOBLvI/AAAAAAAAFiA/xRAHrYEqM_8/s320/Galinha-Pintadinha-png-Queroimagem.com+%281%29.png")
+            };
+            return new ObjectResult(product);
+        }
+    }
+}
---
+++ ApiVue/Model/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiVue.Model
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(string nome, string url)
+        {
+            this.Nome = nome;
+            this.Url = url;
+        }
+
+        public string Nome { get; set; }
+        public string Url { get; set; }
+    }
+}
